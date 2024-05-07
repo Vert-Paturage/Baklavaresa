dffa853fc1e7b9a8d84b6978ef5caec2afbdb7b7
Derya AY <derya.ay26@hotmail.com>
2024-05-07 15:13:43 +0200

    Reservation

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 
-// Import the Home and Admin components
-import { HomeComponent } from './Components/home/home.component';
-import { AdminComponent } from './Components/admin/admin.component';
+import { HomeComponent } from './components/home/home.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 export const routes: Routes = [
 	{ path: '', component: HomeComponent },
 	{ path: 'admin', component: AdminComponent },
-	{ path: '**', redirectTo: '' }
+	{ path: '**', redirectTo: '' },
+
 ];
