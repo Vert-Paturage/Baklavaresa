c40ca15e844be77414ab57748bef2db5e858788e
VakrSb <paulchopinet@gmail.com>
2024-05-22 14:18:16 +0200

    test: save resa

--- server/test/ReservationTest.cs
+++ server/test/ReservationTest.cs
@@ -6,37 +6,49 @@ namespace test;
 
 public class ReservationTest
 {
+    private readonly Reservation _reservationModel =  new()
+    {
+        ID = 1,
+        Date = new Schedule()
+        {
+            Date = "2021-10-01",
+            Time = "12:00"
+        },
+        NumberOfPeople = 2,
+        Tables = new List<Table>()
+        {
+            new()
+            {
+                ID = 1,
+                Capacity = 2
+            }
+        },
+        Email = "email",
+        FirstName = "first",
+        LastName = "last"
+    };
+    
     [Fact]
     public void MailShouldBeSent()
     {
-        var reservationModel = new Reservation()
-       {
-            ID = 1,
-            Date = new Schedule()
-            {
-                Date = "2021-10-01",
-                Time = "12:00"
-            },
-            NumberOfPeople = 2,
-            Tables = new List<Table>()
-            {
-                new()
-                {
-                    ID = 1,
-                    Capacity = 2
-                }
-            },
-            Email = "email",
-            FirstName = "first",
-            LastName = "last"
-        };
         var fakeMailer = new FakeMailer();
-        var dataManipulation = new DataManipulation();
+        var dataManipulation = new FakeDataManipulation();
         var reservation = new ReservationController(fakeMailer,dataManipulation);
         
-        reservation.CreateReservation(reservationModel);
+        reservation.CreateReservation(_reservationModel);
         
         Assert.True(fakeMailer.MailSent);
         Assert.Equal("email", fakeMailer.Email);
+    }
+    [Fact]
+    public void ReservationShouldBeAdded()
+    {
+        var fakeMailer = new FakeMailer();
+        var dataManipulation = new FakeDataManipulation();
+        var reservation = new ReservationController(fakeMailer,dataManipulation);
+        
+        reservation.CreateReservation(_reservationModel);
+        
+        Assert.Equal(_reservationModel, dataManipulation.LastReservation);
     }
 }
