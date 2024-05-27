5e9287572f127a1b4798701ecd8876d1b08d8235
VakrSb <paulchopinet@gmail.com>
2024-05-28 01:54:13 +0200

    delete server dir

--- server/API/Data/IDataManipulation.cs
+++
@@ -1,9 +0,0 @@
-using API.Models.DataModels;
-
-namespace API.Data;
-
-public interface IDataManipulation
-{
-    public void InsertReservation(Reservation reservation);
-    public List<Table> GetTablesByCapacity(int capacity);
-}
