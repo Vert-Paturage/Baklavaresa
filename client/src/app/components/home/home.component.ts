604f26fded37004d78ebc6dccfef840a40440135
Arthur <94467283+avlrd@users.noreply.github.com>
2024-05-29 09:18:12 +0000

    Design page d'accueil + correctif font-weight header

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  templateUrl: "home.component.html",
-  styleUrl: "home.component.css"
+	selector: 'app-home',
+	standalone: true,
+	templateUrl: "home.component.html",
+	styleUrl: "home.component.css",
+	imports: [RouterLink]
 })
 export class HomeComponent {
 }
