b169aaecc4e415031b3e827779aaff73d4e92876
Derya AY <derya.ay26@hotmail.com>
2024-05-31 20:37:06 +0200

    Page admin

--- client/src/app/components/admin/admin.component.ts
+++ client/src/app/components/admin/admin.component.ts
@@ -6,7 +6,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {provideNativeDateAdapter} from '@angular/material/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { format } from 'date-fns';
 import { fr } from 'date-fns/locale';
 
@@ -15,12 +15,13 @@ import { fr } from 'date-fns/locale';
   standalone: true,
   templateUrl: "admin.component.html",
   styleUrl: "admin.component.css",
-  providers: [ApiService, provideNativeDateAdapter()],
+  providers: [ApiService, DatePipe, provideNativeDateAdapter()],
   imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, FormsModule, ReactiveFormsModule, CommonModule],
 })
 
 export class AdminComponent implements OnInit{
 	SelectedDay!: Date;
+  SelectedDayString!: string;
   Reservation: Reservation[] = [
     {
       ID: 6,
@@ -29,33 +30,33 @@ export class AdminComponent implements OnInit{
       Email: "derya@test.fr",
       Date: new Date(),
       NumberOfPeople: 2,
-      NumberOfTables: {ID: 1, Capacity: 4}
+      NumberOfTables: {ID: 1, Capacity: 2}
     }
   ];
 
-  SelectedDayString!: string;
 
-  constructor(private apiService: ApiService) {
-	}
+    constructor(private apiService: ApiService, private datePipe: DatePipe) {
+  	}
+  
 
-  ngOnInit(): void {
+  formatDate(date: string): string {
+    const parsedDate = new Date(date);
+    return format(parsedDate, 'EEEE d MMMM yyyy', { locale: fr });
   }
+
+    ngOnInit(): void {
+    }
 
   onDateChange(event: any): void {
     const selectedDate = event.value;
-    this.apiService.createReservation(this.Reservation[0]).subscribe(() => {
-      console.log("Reservation created");
-    });
+    // this.apiService.createReservation(this.Reservation[0]).subscribe(() => {
+    //   console.log("Reservation created");
+    // });
     this.SelectedDay = selectedDate;
     this.apiService.getCalendarAdmin(this.SelectedDay).subscribe(map => {
       this.Reservation = map;
     });
     this.SelectedDayString = this.formatDate(selectedDate);
-  }
-
-  formatDate(date: string): string {
-    const parsedDate = new Date(date);
-    return format(parsedDate, 'EEEE d MMMM yyyy', { locale: fr });
   }
 
   deleteReservation(index: number) {
