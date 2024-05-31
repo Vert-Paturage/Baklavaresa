9919e5e38a4401c1bd48f02f72d3fdc3fe2bf4ff
VakrSb <paulchopinet@gmail.com>
2024-06-01 01:25:03 +0200

    ça marche pas oskour

--- baklavaresa-backend/tests/Application.UnitTests/Dependencies.cs
+++ baklavaresa-backend/tests/Application.UnitTests/Dependencies.cs
@@ -7,11 +7,14 @@ namespace Application.UnitTests;
 public class Dependencies
 {
     protected readonly IServiceCollection Services;
+    public IServiceProvider ServiceProvider { get; private set; }
     public Dependencies()
     {
         Services = new ServiceCollection();
         
         Services.ApplicationMediator();
         Services.AddScoped<IEmailService, FakeEmailService>();
+        
+        ServiceProvider = Services.BuildServiceProvider();
     }
 }
--- baklavaresa-backend/tests/Application.UnitTests/ReservationCreation.cs
+++ baklavaresa-backend/tests/Application.UnitTests/ReservationCreation.cs
@@ -1,6 +1,13 @@
+using Domain.Repositories;
+
 namespace Application.UnitTests;
 
 public class ReservationCreation: IClassFixture<Dependencies>
 {
-    
+   private readonly IReservationRepository _reservationRepository;
+   
+   public ReservationCreation(Dependencies dependencies)
+   {
+       _reservationRepository = dependencies.;
+   }
 }
