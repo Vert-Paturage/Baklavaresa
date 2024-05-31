fab76925de1cb715579b82a1fc8e5fe8aea75204
VakrSb <paulchopinet@gmail.com>
2024-05-31 23:53:58 +0200

    bha les includes partout

--- baklavaresa-backend/src/Infrastructure/Data/Repositories/ReservationRepository.cs
+++ baklavaresa-backend/src/Infrastructure/Data/Repositories/ReservationRepository.cs
@@ -2,6 +2,7 @@ using Domain.Entities;
 using Domain.Repositories;
 using Infrastructure.Data.Entities;
 using Infrastructure.Data.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data.Repositories;
 
@@ -25,20 +26,26 @@ public class ReservationRepository(Persistence.DatabaseContext context) : IReser
     }
     public Task<IList<Reservation>> GetAllForMonth(DateTime month)
     {
-        var dbReservation = _context.Reservations.Where(r => r.Date.Month == month.Month).ToList();
+        var dbReservation = _context.Reservations
+                .Include(r => r.Table)
+                .Where(r => r.Date.Month == month.Month).ToList();
         return Task.FromResult<IList<Reservation>>(dbReservation.Select(r => r.ToDomainModel()).ToList());
     }
 
     public Task<IList<Reservation>> GetReservationsByDateAdmin(DateTime slot)
     {
-        var dbReservation = _context.Reservations.Where(r => r.Date.Date == slot.Date).ToList();
+        var dbReservation = _context.Reservations
+            .Include(r => r.Table)
+            .Where(r => r.Date.Date == slot.Date).ToList();
         return Task.FromResult<IList<Reservation>>(dbReservation.Select(r => r.ToDomainModel()).ToList());
     }
 
      public Task<IList<Reservation>> GetReservationsByDate(DateTime slot)
     {
         var slotEnd = slot.AddHours(1);
-        var dbReservation = _context.Reservations.Where(r => r.Date >= slot && r.Date < slotEnd).ToList();
+        var dbReservation = _context.Reservations.
+            Include(r => r.Table)
+            .Where(r => r.Date >= slot && r.Date < slotEnd).ToList();
         return Task.FromResult<IList<Reservation>>(dbReservation.Select(r => r.ToDomainModel()).ToList());
     }
 
