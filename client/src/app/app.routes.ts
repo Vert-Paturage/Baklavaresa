5fa69484a2a4038779ad2fbaff51d3ad77f78b06
Florian Spindler <florian.spindler@etu.unistra.fr>
2024-06-01 16:52:33 +0200

    Ajout Secret

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { ReservationComponent } from './components/reservation/reservation.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 export const routes: Routes = [
 	{ path: '', component: HomeComponent },
 	{ path: 'admin', component: AdminComponent },
 	{ path: 'reservation', component: ReservationComponent },
+	{ path: 'menu', component: MenuComponent},
 	{ path: '**', redirectTo: '', pathMatch: 'full'}
 ];
