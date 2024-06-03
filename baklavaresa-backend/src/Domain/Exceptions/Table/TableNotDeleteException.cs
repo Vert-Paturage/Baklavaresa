b56e2964adaf629ff69d697ef48334fc637cbf76
Derya AY <derya.ay26@hotmail.com>
2024-06-03 11:10:58 +0200

    Erreur exception table

--- baklavaresa-backend/src/Domain/Exceptions/Table/TableNotDeleteException.cs
+++ baklavaresa-backend/src/Domain/Exceptions/Table/TableNotDeleteException.cs
@@ -2,7 +2,7 @@ namespace Domain.Exceptions.Table;
 
 public class TableNotDeleteException: Exception
 {
-    public TableNotDeleteException(): base($"Table could not be deleted.")
+    public TableNotDeleteException(): base($"La table ne peut pas être supprimée car elle a des réservations associées.")
     {
     } 
 }
