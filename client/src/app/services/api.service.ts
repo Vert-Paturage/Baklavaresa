4eed3399927adc640045347bcf0fc9a312973836
Arthur <villardarthur@gmail.com>
2024-06-03 23:03:22 +0200

    Fixed css now it's beautiful

--- client/src/app/services/api.service.ts
+++ client/src/app/services/api.service.ts
@@ -73,26 +73,4 @@ export class ApiService {
 	getAllTables(): Observable<Table[]> {
 		return this.http.get<Table[]>('/api/Table/GetAll');
 	}
-
-	getCalendarStub(calendar: Calendar): Map<Date, Date[]> {
-		// 1st of may 2024
-		const date: Date = new Date(2024, 4, 1);
-
-		const offset: number = Math.floor(Math.random() * 7);
-
-		const stub: Map<Date, Date[]> = new Map();
-		for (let i = 1; i <= 31; i++) {
-			const toSet: Date = new Date(date.getFullYear(), date.getMonth(), i);
-			stub.set(toSet, []);
-			for (let j = 0; j < Math.floor(Math.random() * 5); j++) {
-				stub.get(toSet)!.push(this.getRandomSchedule(i));
-			}
-		}
-		return stub;
-	}
-
-	private getRandomSchedule(day: number): Date {
-		const date: Date = new Date();
-		return new Date(date.getFullYear(), date.getMonth(), day, Math.floor(Math.random() * 24), Math.floor(Math.random() * 60));
-	}
 }
