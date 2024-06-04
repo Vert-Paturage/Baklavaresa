1756ca71051d3bafc839a8af7b62d216d0fb8d17
Florian Spindler <florian.spindler@etu.unistra.fr>
2024-06-04 11:20:06 +0200

    add some test

--- baklavaresa-frontend/src/app/components/reservation/reservation.component.spec.ts
+++ baklavaresa-frontend/src/app/components/reservation/reservation.component.spec.ts
@@ -1,17 +1,37 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
 import { ReservationComponent } from './reservation.component';
+import { CalendarComponent } from '../calendar/calendar.component';
+import { ScheduleSelectorComponent } from '../scheduleselector/scheduleselector.component';
+import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
+import { ApiService } from '../../services/api.service';
+import { of } from 'rxjs';
+import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 describe('ReservationComponent', () => {
   let component: ReservationComponent;
   let fixture: ComponentFixture<ReservationComponent>;
+  let apiService: jasmine.SpyObj<ApiService>;
+  let router: Router;
 
   beforeEach(async () => {
+    const apiServiceSpy = jasmine.createSpyObj('ApiService', ['getCalendar']);
+
     await TestBed.configureTestingModule({
-      imports: [ReservationComponent]
-    })
-    .compileComponents();
-    
+      imports: [
+        RouterTestingModule,
+        HttpClientModule, // Add HttpClientModule
+        ReservationComponent,
+        CalendarComponent,
+        ScheduleSelectorComponent
+      ],
+      providers: [{ provide: ApiService, useValue: apiServiceSpy }]
+    }).compileComponents();
+
+    apiService = TestBed.inject(ApiService) as jasmine.SpyObj<ApiService>;
+    router = TestBed.inject(Router);
     fixture = TestBed.createComponent(ReservationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +40,23 @@ describe('ReservationComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+
+  it('should update SelectedDay on date selection', () => {
+    const day = { day: new Date(), slots: [] };
+    component.onDateSelected(day);
+    expect(component.SelectedDay).toBe(day);
+  });
+
+  it('should update SelectedSchedule on schedule selection', () => {
+    const schedule = new Date();
+    component.onScheduleSelected(schedule);
+    expect(component.SelectedSchedule).toBe(schedule);
+  });
+
+  });
+
+    @Component({
+      template: ''
+    })
+    class DummyComponent {}
+
