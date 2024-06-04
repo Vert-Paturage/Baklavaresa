83a0f5b6459d0f612acb4ae55a4815b5fc19aaf5
Derya AY <derya.ay26@hotmail.com>
2024-06-04 18:10:10 +0200

    Ajout message d'erreur qui remontent du back

--- baklavaresa-frontend/src/app/components/contactinfo/contactinfo.component.ts
+++ baklavaresa-frontend/src/app/components/contactinfo/contactinfo.component.ts
@@ -8,6 +8,7 @@ import { ApiService } from '../../services/api.service';
 import ReservationRequest from "../../types/reservationRequest";
 
 import { getUTCISOString } from "../../utils/dateStringHandler";
+import { SnackbarService } from "../../services/snackbar.service";
 
 @Component({
   standalone: true,
@@ -29,13 +30,12 @@ export class ContactInfoComponent {
   private selectedDate: Date = new Date();
 
 
-  constructor(private api: ApiService, private router: Router) {
+  constructor(private api: ApiService, private router: Router, private snackBar: SnackbarService) {
     const navigation = this.router.getCurrentNavigation();
     console.log("Navigation: ", navigation);
     if (navigation != null) {
       const nav = navigation.extractedUrl;
       if(nav != null) {
-        // this.Reservation.date = nav.queryParams['state'];
         this.selectedDate = nav.queryParams['state'];
         this.Reservation.numberOfPeople = nav.queryParams['people'];
       }
@@ -43,15 +43,27 @@ export class ContactInfoComponent {
   }
 
   submitReservation() {
-    if (this.Reservation.numberOfPeople > 0 && this.Reservation.firstName != null && this.Reservation.lastName != null && this.Reservation.email) {
-	  let date = new Date(this.selectedDate);
+    if (this.Reservation.numberOfPeople < 0 || this.Reservation.firstName == null || this.Reservation.lastName == null || this.Reservation.email == null || this.Reservation.date == null) {
+      this.snackBar.showSnackbar("Veuillez remplir tous les champs", 'error');
+    }
+    else {
+      let date = new Date(this.selectedDate);
       this.Reservation.date = getUTCISOString(date);
-      console.log("Reservation: ", this.Reservation);
-	  this.Reservation.email = this.Reservation.email.toLowerCase();
-      if (this.Reservation != null) {
-        const response = this.api.createReservation(this.Reservation).subscribe((response: string) => {
-          console.log("Reservation created: ", response);
-        })
+      this.Reservation.email = this.Reservation.email.toLowerCase();
+        if (this.Reservation != null) {
+          this.api.createReservation(this.Reservation).subscribe(
+            (response) => {
+              this.snackBar.showSnackbar("Réservation ajoutée avec succès", 'success');
+              this.Reservation = {
+                firstName: "",
+                lastName: "",
+                email: "",
+                date: "",
+                numberOfPeople: 0,
+              };
+            },
+            (error) => this.snackBar.showSnackbar(error.error, 'error')
+          );
       }
     }
   }
